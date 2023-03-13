5fc53e8f6f51f31237f1cf7ba7a7039bc06aad44
Christian Aquino <christianaquinomoreta2@gmail.com>
2023-03-13 18:33:11 -0400

    .

--- database.py
+++ database.py
@@ -4,11 +4,5 @@ import configuration as C
 myclient = MongoClient(C.CON_STRING)
 mydb = myclient[C.CON_CLIENT]
 
-#myview = mydb["Consulta 3"]
-#cursor = myview.find()
-
-#for result in cursor:
-#  print(result)
-
 if __name__ == "__main__":
     dbname= mydb
